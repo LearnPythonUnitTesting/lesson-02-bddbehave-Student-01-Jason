7c3985b3918e5e5c27518da0cced524c5b417b05
Student-01-Jason <63417122+Student-01-Jason@users.noreply.github.com>
2020-07-18 23:55:04 +0100

    Update check.py

--- features/steps/check.py
+++ features/steps/check.py
@@ -2,5 +2,15 @@ from behave import *
 
 
 @Given('I have a word {a:w}')
-#TODO
+def step_impl(context, a):
+    context.a = a
 
+
+@When('I input a word {b:w}')
+def step_impl(context, b):
+    context.b = b
+
+
+@Then('the input word is the capital of target word')
+def step_impl(context):
+    assert context.a.upper() == context.b
